0d23fdf57fd47403974aa01d58e63518d55a749d
Yash Pawar <pawaryr28@gmail.com>
2023-10-16 02:33:30 +0530

    first commit

---
+++ preprocessor.py
@@ -0,0 +1,38 @@
+import pandas as pd
+import re
+
+def preprocess(data):
+    pattern = '\d{1,2}\/\d{1,2}\/\d{2},\s\d{1,2}:\d{2}\s(?:am|pm)\s-\s'
+    messages = re.split(pattern, data)[1:]
+    dates = re.findall(pattern, data)
+    df = pd.DataFrame({'user_message': messages, 'message_date': dates})
+    df['message_date'] = df['message_date'].str.replace('\u2009', ' ')
+    df['date'] = pd.to_datetime(df['message_date'], format='%d/%m/%y, %I:%M %p - ')
+    users = []
+    messages = []
+    for message in df['user_message']:
+        entry = re.split('([\w\W]+?):\s', message)
+        if entry[1:]:  # user name
+            users.append(entry[1])
+            messages.append(" ".join(entry[2:]))
+        else:
+            users.append('group_notification')
+            messages.append(entry[0])
+
+    df['user'] = users
+    df['message'] = messages
+    df.drop(columns=['user_message'], inplace=True)
+
+    df['only_date'] = df['date'].dt.strftime('%y-%m-%d')
+    df['year'] = df['date'].dt.year
+    df['month_num'] = df['date'].dt.month
+    df['month'] = df['date'].dt.month_name()
+    df['day'] = df['date'].dt.day
+    df['day_name'] = df['date'].dt.day_name()
+    df['hour'] = df['date'].dt.strftime('%I')
+    df['minute'] = df['date'].dt.minute
+
+    df['date'] = df['date'].dt.strftime('%d/%m/%y, %I:%M %p')
+    df = df.drop(columns=['message_date'])
+
+    return df
